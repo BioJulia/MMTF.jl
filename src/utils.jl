135b028e223b0773b3970a3b4ab90bdbbf11b680
Joel S <joel.programmer@outlook.com>
2018-08-09 05:20:02 +0530

    Support for Julia v0.7

--- src/utils.jl
+++ src/utils.jl
@@ -1,8 +1,8 @@
-function Base.hton(array::Array{T,1}) where {T<:Union{Real,Char}}
+function htonofarray(array::AbstractArray{T,1}) where {T<:Union{Real,Char}}
     return [hton(x) for x in array]
 end
 
-function Base.ntoh(array::Array{T,1}) where {T<:Union{Real,Char}}
+function ntohofarray(array::AbstractArray{T,1}) where {T<:Union{Real,Char}}
     return [ntoh(x) for x in array]
 end
 
@@ -12,7 +12,7 @@ Returns the codec, the length of the decoded array, the parameter and the remain
 of the array
 """
 function parseheader(input_array::Array{UInt8,1})
-    codec, length, param = hton(reinterpret(Int32,input_array[1:12]))
+    codec, length, param = htonofarray(reinterpret(Int32,input_array[1:12]))
     return codec,length,param,input_array[13:end]
 end
 
@@ -20,7 +20,9 @@ end
 Add the header to the appropriate array
 """
 function addheader(input_array::Array{UInt8,1}, codec::Integer, length::Integer, param::Integer)
-    return append!(reinterpret(UInt8,[ntoh(Int32(codec))]), 
-    append!(reinterpret(UInt8,[ntoh(Int32(length))]), 
-    append!(reinterpret(UInt8,[ntoh(Int32(param))]), input_array)))
+    out_array=UInt8[]
+    append!(out_array, reinterpret(UInt8,[ntoh(Int32(codec))])) 
+    append!(out_array, reinterpret(UInt8,[ntoh(Int32(length))])) 
+    append!(out_array, reinterpret(UInt8,[ntoh(Int32(param))]))
+    append!(out_array, input_array)
 end
